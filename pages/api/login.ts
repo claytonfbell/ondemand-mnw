1361334404b2710bdf1b20edeb5f64da352d9283
Clay Bell <clay.bell@etix.com>
2023-03-27 12:57:42 -0700

    fix: updated approved admin list

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -54,7 +54,6 @@ async function handler(
           "nick@montessori-nw.org",
           "angelika@montessori-nw.org",
           "claytonfbell@gmail.com",
-          "emma@montessori-nw.org",
         ]
         if (approvedAdmins.includes(email)) {
           user = await prisma.user.create({ data: { email, isAdmin: true } })
