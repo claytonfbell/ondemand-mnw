d7340ddc939e8b283b226efbb3faf59334433737
Clay Bell <clay.bell@etix.com>
2024-09-13 14:33:43 -0700

    chore: added tyler

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -54,6 +54,7 @@ async function handler(
           "nick@montessori-nw.org",
           "angelika@montessori-nw.org",
           "claytonfbell@gmail.com",
+          "tyler@montessori-nw.org",
         ]
         if (approvedAdmins.includes(email)) {
           user = await prisma.user.create({ data: { email, isAdmin: true } })
